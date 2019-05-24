f6c6d67bb58b979d0f72582a50c2b99b1370c586
Drew Facchiano <drewfacchiano@Drews-MacBook-Pro.local>
2019-05-24 16:12:31 -0700

    Webpage
    Updated webpage to reflect references
    Added Berky css to app

--- examples/js/gotchas.js
+++ examples/js/gotchas.js
@@ -10,16 +10,12 @@ Ninja.prototype = {};
 
 Utils.assert(!(ninja instanceof Ninja), "INSTANCEOF: The ninja is now not a Ninja! This demonstrates a gotch of instanceOf! Instance of checks through the prototype chain. When we set Ninja's prototype to an empty object. That effected what 'instance' we were looking at.");
 
-
 // Instance vs 'static'
 function StaticNinja(){}
 StaticNinja.message = "Hello";
 
-const normmalNinja = new StaticNinja();
-
-const a1 = normmalNinja.message;
-const a2 = StaticNinja.message;
+const normalNinja = new StaticNinja();
 
 //message is NOT tied to instance
-Utils.assert(!(a1==='Hello'),"STATIC: Message was applied to the StaticNinja constructor NOT the instance. Imagine a static method");
-Utils.assert(a2 === 'Hello', "STATIC: When called on the constructor it's self we can see the message")
+Utils.assert(!(normalNinja.message ==='Hello'),"STATIC: Message was applied to the StaticNinja constructor NOT the instance. Imagine a static method");
+Utils.assert(StaticNinja.message === 'Hello', "STATIC: When called on the constructor it's self! As a result we can see the message")
