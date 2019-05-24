63df9ae147040cf15797c7ec1dc84cd4616fb214
Drew Facchiano <drewfacchiano@Drews-MacBook-Pro.local>
2019-05-24 12:26:48 -0700

    Additional JS examples
    Adding example for Apply_Call_bind
    Adding example for Closures
    Adding example for RestParams vs Arguments

---
+++ examples/js/closures.js
@@ -0,0 +1,40 @@
+const Utils = require('./utils');
+
+
+let makeCounter = function(value) {
+    let privatecounter = value;
+    function changeBy(val) {
+        privatecounter += val;
+    };
+    function setCounter(val) {
+        privatecounter = val;
+    }
+    return {
+        increment: function() {
+            changeBy(1);
+        },
+        decrement: function(){
+            changeBy(-1);
+        },
+        value: function() {
+            return privatecounter;
+        },
+        setValue: function(val) {
+            return setCounter(val);
+        },
+        sums: function(x){
+            return function callBack(y){
+                return x + y;
+            }
+        }
+    }
+};
+
+let counter = makeCounter(15);
+Utils.assert(counter.value() === 15, "A counter object is made using a constructor");
+counter.increment();
+Utils.assert(counter.value() === 16, "We can access 'private' values using a closure. The values are stored within but can be access from out of the scope");
+counter.setValue(115);
+Utils.assert(counter.value()=== 115, "A common example of this might be getters/setters for private variables");
+let callMe = counter.sums(10);
+Utils.assert(callMe(5) === 15, "We can also use closures to set callback functions");
