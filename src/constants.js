6917721adb548372da59b55944378fdc068833f7
Drew Facchiano <andrewfacchino@gmail.com>
2019-07-28 18:55:11 -0700

    Topic component
    Adding topic component for easier iteration

---
+++ src/constants.js
@@ -0,0 +1,105 @@
+const TOPICS = {
+  Lodash: {
+    title: "Lodash",
+    topics: [
+      {
+        title: "All purpose reminder",
+        url: "https://devhints.io/lodash"
+      },
+      {
+        title: "Useful fuctions",
+        url:
+          "https://colintoh.com/blog/lodash-10-javascript-utility-functions-stop-rewriting"
+      },
+      {
+        title: "Docs",
+        url: "https://lodash.com/docs/4.17.11"
+      },
+      {
+        title: "Implementation Ref",
+        url: "https://youmightnotneed.com/lodash/"
+      }
+    ]
+  },
+  Testing: {
+    title: "Testing",
+    topics: [
+      {
+        title: "Mocha",
+        url: "https://mochajs.org/"
+      },
+      {
+        title: "Reference guide",
+        url:
+          "https://blog.logrocket.com/a-quick-and-complete-guide-to-mocha-testing-d0e0ea09f09d"
+      },
+      {
+        title: "Mocha Cheat Sheet",
+        url: "https://devhints.io/mocha"
+      }
+    ]
+  },
+  CSS_GENERAL: {
+    title: "",
+    topics: [
+      {
+        title: "Traditional Reference page",
+        url: "https://devhints.io/css"
+      },
+      {
+        title: "Flexbox Guide",
+        url: "https://css-tricks.com/snippets/css/a-guide-to-flexbox/"
+      },
+      {
+        title: "Grid Garden Game",
+        url: "https://cssgridgarden.com/"
+      },
+      {
+        title: "Flex Zombies Game",
+        url: "https://mastery.games/p/flexbox-zombies"
+      }
+    ]
+  },
+  GIT_GENERAL: {
+    title: "",
+    topics: [
+      {
+        title: "Creating a new repo",
+        url:
+          "https://help.github.com/en/articles/adding-an-existing-project-to-github-using-the-command-line"
+      },
+      {
+        title: "Git cheat sheet",
+        url: "https://www.git-tower.com/blog/git-cheat-sheet"
+      },
+      {
+        title: "Command Reference",
+        url:
+          "https://github.github.com/training-kit/downloads/github-git-cheat-sheet.pdf"
+      }
+    ]
+  },
+  PYTHON_GENERAL: {
+    title: "",
+    topics: [
+      {
+        title: "Mosh basics page",
+        url: "https://programmingwithmosh.com/python/python-3-cheat-sheet/"
+      },
+      {
+        title: "General reference page",
+        url: "https://www.pythonsheets.com/"
+      },
+      {
+        title: "Standards page",
+        url: "https://www.pythonsheets.com/notes/python-code-style.html"
+      },
+      {
+        title: "Twitter bot project",
+        url: "https://www.digitalocean.com/community/tutorials/how-to-create-a-twitterbot-with-python-3-and-the-tweepy-library"
+      }
+    ]
+  }
+};
+
+export default TOPICS;
